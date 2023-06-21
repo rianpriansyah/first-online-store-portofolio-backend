947049a25065d38e82f2e46dfd81380a715c910e
Rian Priansyah <rian.priansyah04@gmail.com>
2023-06-21 11:04:04 +0700

    membuat fungsi umum dari query database

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -34,6 +34,61 @@ class MY_Model extends CI_Model {
         return $this->form_validation->run();
     }
 
+    public function select ($columns) {
+        $this->db->select($columns);
+        return $this;
+    }
+
+    public function where ($column, $condition) {
+        $this->db->where($column, $condition);
+        return $this;
+    }
+
+    public function like ($column, $condition) {
+        $this->db->like($column, $condition);
+        return $this;
+    }
+
+    public function orLike ($column, $condition) {
+        $this->db->or_like($column, $condition);
+        return $this;
+    }
+
+    public function join ($table, $type = 'left') {
+        $this->db->join($table, "$this->table.id_table = $table.id", $type);
+        return $this;
+    }
+
+    public function orderBy ($column, $order = 'asc') {
+        $this->db->order_by($column, $order);
+        return $this;
+    }
+
+    public function first () {
+        return $this->db->get($this->table)->row();
+    }
+
+    public function get () {
+        return $this->db->get($this->table)->result();
+    }
+
+    public function count () {
+        return $this->db->count_all_results($this->table);
+    }
+
+    public function create ($data) {
+        $this->db->insert($this->table, $data);
+        return $this->db->insert_id();
+    }
+
+    public function update ($data) {
+        return $this->db->update($this->table, $data);
+    }
+
+    public function delete () {
+        $this->db->delete($this->table);
+        return $this->db->affected_rows();
+    }
 }
 
 /* End of file MY_Model.php */
