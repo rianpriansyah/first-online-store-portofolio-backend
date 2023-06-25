4c4230f8f40fa777824291066e7caf954c10ac0a
Rian Priansyah <rian.priansyah04@gmail.com>
2023-06-25 12:31:17 +0700

    membuat controller kategori, fungsi index dan mengupdate file Routes.php

---
+++ application/controllers/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Controller extends MY_Controller {
+
+    
+    public function __construct()
+    {
+        parent::__construct();
+        
+    }
+    
+
+    public function index($page = null)
+    {
+        $data['title'] = 'Admin: Category';
+        $data['content'] = $this->category->paginate($page)->get();
+        $data['total_rows'] = $this->category->count();
+        $data['pagination'] = $this->category->makePagination(base_url('category'), 2, $data['total_rows']);
+        $data['page'] = 'pages/category/index';
+
+        $this->view($data);
+    }
+
+}
+
+/* End of file Controller.php */
