10554d61f94e2edaf19dd2020c6e0879599df9de
Rian Priansyah <rian.priansyah04@gmail.com>
2023-07-03 14:17:49 +0700

    menampilkan produk berdasarkan kategori

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,7 +10,7 @@ class Home extends MY_Controller {
         $data['content'] = $this->home->select(
             [
                 'product.id', 'product.title AS product_title', 'product.description', 'product.image',
-                'product.price', 'product.is_available', 'category.title AS category_title'
+                'product.price', 'product.is_available', 'category.title AS category_title', 'category.slug AS category_slug'
             ]
         )
         ->join('category')
