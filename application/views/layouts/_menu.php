c644c1c885957fd768d5bb299114074a3bb7da3a
Rian Priansyah <rian.priansyah04@gmail.com>
2023-07-03 11:43:39 +0700

    membuat fungsi ubah data pengguna

--- application/views/layouts/_menu.php
+++ application/views/layouts/_menu.php
@@ -2,7 +2,7 @@
     <div class="card-header">Menu</div>
     <div class="list-group list-group-flush">
         <li class="list-group-item">
-            <a href="/profile.html">Profile</a>
+            <a href="<?= base_url("/profile"); ?>">Profile</a>
         </li>
         <li class="list-group-item">
             <a href="/orders.html">Orders</a>
