957a58b91fc0a6e7cac3d87f37836006565a5eca
Rian Priansyah <rian.priansyah04@gmail.com>
2023-07-03 12:18:21 +0700

    menampilkan data produk di beranda

--- application/views/pages/home/index.php
+++ application/views/pages/home/index.php
@@ -14,14 +14,15 @@
             </div>
           </div>
           <div class="row">
+            <?php foreach($content as $row): ?>
             <div class="col-md-6">
               <div class="card mb-3">
-                <img src="https://placehold.co/100x70" alt="" class="card-img-top" />
+                <img src="<?= $row->image ? base_url("/images/product/$row->image") : base_url("/images/product/default.png"); ?>" alt="" height="" class="card-img-top" />
                 <div class="card-body">
-                  <h5 class="card-title">Product Title</h5>
-                  <p class="card-text"><strong>Rp. 100.000,-</strong></p>
-                  <p class="card-text">Some quick example text to build product card.</p>
-                  <a href="#" class="badge badge-primary"><i class="fas fa-tags"></i> Kategory</a>
+                  <h5 class="card-title"><?= $row->product_title ?></h5>
+                  <p class="card-text"><strong>Rp<?= number_format($row->price, 0, ',', '.'); ?>,-</strong></p>
+                  <p class="card-text"><?= $row->description ?></p>
+                  <a href="#" class="badge badge-primary"><i class="fas fa-tags"></i> <?= $row->category_title ?></a>
                 </div>
                 <div class="card-footer">
                   <form action="">
@@ -35,78 +36,10 @@
                 </div>
               </div>
             </div>
-            <div class="col-md-6">
-              <div class="card mb-3">
-                <img src="https://placehold.co/100x70" alt="" class="card-img-top" />
-                <div class="card-body">
-                  <h5 class="card-title">Product Title</h5>
-                  <p class="card-text"><strong>Rp. 100.000,-</strong></p>
-                  <p class="card-text">Some quick example text to build product card.</p>
-                  <a href="#" class="badge badge-primary"><i class="fas fa-tags"></i> Kategory</a>
-                </div>
-                <div class="card-footer">
-                  <form action="">
-                    <div class="input-group">
-                      <input type="number" class="form-control" />
-                      <div class="input-group-append">
-                        <button class="btn btn-primary">Add to Cart</button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="card mb-3">
-                <img src="https://placehold.co/100x70" alt="" class="card-img-top" />
-                <div class="card-body">
-                  <h5 class="card-title">Product Title</h5>
-                  <p class="card-text"><strong>Rp. 100.000,-</strong></p>
-                  <p class="card-text">Some quick example text to build product card.</p>
-                  <a href="#" class="badge badge-primary"><i class="fas fa-tags"></i> Kategory</a>
-                </div>
-                <div class="card-footer">
-                  <form action="">
-                    <div class="input-group">
-                      <input type="number" class="form-control" />
-                      <div class="input-group-append">
-                        <button class="btn btn-primary">Add to Cart</button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="card mb-3">
-                <img src="https://placehold.co/100x70" alt="" class="card-img-top" />
-                <div class="card-body">
-                  <h5 class="card-title">Product Title</h5>
-                  <p class="card-text"><strong>Rp. 100.000,-</strong></p>
-                  <p class="card-text">Some quick example text to build product card.</p>
-                  <a href="#" class="badge badge-primary"><i class="fas fa-tags"></i> Kategory</a>
-                </div>
-                <div class="card-footer">
-                  <form action="">
-                    <div class="input-group">
-                      <input type="number" class="form-control" />
-                      <div class="input-group-append">
-                        <button class="btn btn-primary">Add to Cart</button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
+            <?php endforeach ?>
           </div>
           <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item"><a href="#" class="page-link">Previous</a></li>
-              <li class="page-item"><a href="#" class="page-link">1</a></li>
-              <li class="page-item"><a href="#" class="page-link">2</a></li>
-              <li class="page-item"><a href="#" class="page-link">3</a></li>
-              <li class="page-item"><a href="#" class="page-link">Next</a></li>
-            </ul>
+            <?= $pagination ?>
           </nav>
         </div>
         <div class="col-md-3">
