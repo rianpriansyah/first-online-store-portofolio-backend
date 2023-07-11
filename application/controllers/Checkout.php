07c2ee8910a7a8d080c698edd2bcd2fa8618076f
Rian Priansyah <rian.priansyah04@gmail.com>
2023-07-11 15:14:02 +0700

    membuat tabel, model dan controller checkout

---
+++ application/controllers/Checkout.php
@@ -0,0 +1,29 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Checkout extends MY_Controller {
+
+    private $id;
+
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $is_login = $this->session->userdata('is_login');
+        $this->id = $this->session->userdata('id');
+
+        if (!$is_login) {
+            redirect(base_url());
+            return;
+        }
+    }
+
+    public function index()
+    {
+        
+    }
+
+}
+
+/* End of file Checkout.php */
